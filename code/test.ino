5385d2b9f7b2c7d918f33a977380d55737440343
Ni-ChiTing <dfdf22222@gmail.com>
2017-10-15 01:05:42 +0800

    arduino code
    arduino code

---
+++ code/test.ino
@@ -0,0 +1,12 @@
+int mic=A0;
+int micout;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  micout=analogRead(mic);
+  Serial.println(micout);
+}
