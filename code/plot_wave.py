7c424553583fd6e52e995521d6bc006f36989827
siahuat0727 <tansiahuat@gmail.com>
2017-10-18 18:07:33 +0800

    plot new wave

--- code/plot_wave.py
+++ code/plot_wave.py
@@ -14,21 +14,21 @@ def plot(location, file_name):
             plt.plot(data)
             # plt.show()
             graph_path = path[:-3] + "png"
-            plt.savefi(graph_path)
+            plt.savefig(graph_path)
             plt.clf()
-            print(graph_path + "is plotted")
+            print(graph_path + " is plotted")
 
 def plot2(location, x, y):
-    file_type = ".txt"
-    file_name = str(x) + "-" + str(y) + file_type
+    file_type = ".log"
+    file_name = str(x) + "-" + str(y) + "_new"  + file_type
     plot(location, file_name)
 
 def plotAll():
-    folder_name = ["no_leak_new", "leak_new"]
+    folder_name = ["no_leak", "leak"]
     for location in ["../" + str(x) + "/" for x in folder_name]:
         for x, y in itertools.product(range(1,8), range(0, 100, 20)):
             plot2(location, x, y)
-        plot(location, "open_close.txt");
+        plot(location, "open_close_new.log");
 
 plotAll()
 # plot("../no_leak_new/", "open_close.txt")
