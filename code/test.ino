68277417cf00c5f8d2f8582817a6783b26221600
Ni-ChiTing <dfdf22222@gmail.com>
2017-10-20 02:45:28 +0800

    second version test
    改變取樣率

--- code/test.ino
+++ code/test.ino
@@ -1,12 +1,51 @@
+/*unsigned long time;
+unsigned long endtime;
 int mic=A0;
 int micout;
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
+void setup(){
+  Serial.begin(57600);
+  for(int i=0;i<1000;i++)
+  {
+    micout=analogRead(mic);
+  }
 }
+void loop(){
+  time = millis();
+  //prints time since program started
+    for(int i=0;i<1000;i++)
+  {
+    micout=analogRead(mic);
+     Serial.print ( micout);
+  }
+  endtime=millis();
+  unsigned long slap= endtime-time;
+  Serial.println();
+  Serial.print(slap);
+   Serial.println();
+  // wait a second so as not to send massive amounts of data
+  delay(5000);
+}*/const int pin = A0;
+ unsigned long n =50000;  // sample 採樣 1000 次
+void setup() {
+  Serial.begin(115200);
+  setP16( ) ;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  micout=analogRead(mic);
-  Serial.println(micout);
+}
+void loop( ) { // 
+  unsigned long begt, runt, total;
+  //total = 0;  // clear before sampling
+  //begt = micros();
+    total = analogRead(pin);
+    Serial.print(total);
+  //runt = micros() - begt;  // elapsed time
+  //Serial.println();
+  //Serial.print(String("Time per sample: ")+runt/1.0/n +"us");
+  //Serial.println(String(", Frequency: ")+1000000.0/runt*n +" Hz");
+  //delay(5566);
+}// loop(
+void setP16( ) {
+  Serial.println("ADC Prescaler = 16");  // 100
+  ADCSRA |=  (1 << ADPS2);   // 1
+  ADCSRA &=  ~(1 << ADPS1);  // 0
+  ADCSRA &=  ~(1 << ADPS0);  // 0
 }
