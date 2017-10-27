19db0fb4095872079639747fa9d96f0d1fc05176
Ni-ChiTing <dfdf22222@gmail.com>
2017-10-27 09:34:54 +0800

    測量水壓v2
    修訂起始值

--- code/pressure.ino
+++ code/pressure.ino
@@ -1,6 +1,6 @@
 const unsigned long MIN_PRESSURE = 0UL;
 const unsigned long MAX_PRESSURE = 1200000UL;  // 1.2 MPa
-const int MIN_READING = 0;
+const int MIN_READING = 1024 * 0.5 / 5;
 const int MAX_READING = 1024 * 4.5 / 5; 
 void setup() {
   // put your setup code here, to run once:
@@ -9,7 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long pressure_in_Pascals = map(analogRead(A0), MIN_READING, MAX_READING, MIN_PRESSURE, MAX_PRESSURE);//map(value, fromLow, fromHigh, toLow, toHigh)
+  unsigned long pressure_in_Pascals = map(analogRead(A0), MIN_READING, MAX_READING, MIN_PRESSURE, MAX_PRESSURE);//map(value, fromLow, fromHigh, toLow, toHigh);
   //Re-maps a number from one range to another. That is, a value of fromLow would get mapped to toLow, a value of fromHigh to toHigh, values in-between to values in-between, etc.
   Serial.print("pressure: ");
   Serial.println(pressure_in_Pascals);
